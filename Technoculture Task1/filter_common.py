b68fa3d654547262f6d47fc66ca2c8a8595f9940
palak178 <70845267+palak178@users.noreply.github.com>
2023-06-16 19:27:34 +0530

    Update filter_common.py

--- Technoculture Task1/filter_common.py
+++ Technoculture Task1/filter_common.py
@@ -4,13 +4,16 @@ import pandas as pd
 df = pd.read_csv('common.csv')
 
 # Apply the first filter to select desired columns
-filtered_df = df[['Name', 'Sequence', 'Biomarker_Name']]
+filtered_df = df[['Name', 'Sequence', 'Biomarker_Name', 'Sampling_Method', 'Collection_site', 'Disease_name', 'Regulation', 'Exosomal/Non-exosomal']]
 
 # Apply the second filter to keep rows with the same 'Name' and 'Biomarker_Name'
 filtered_df = filtered_df[filtered_df['Name'] == filtered_df['Biomarker_Name']]
 
 # Apply the third filter to remove the 'Name' column
 filtered_df = filtered_df.drop('Name', axis=1)
+
+# Keep only one row for each unique 'Biomarker_Name'
+filtered_df = filtered_df.drop_duplicates(subset='Biomarker_Name')
 
 # Save the final filtered DataFrame to a new CSV file
 filtered_df.to_csv('final_filtered_common.csv', index=False)
