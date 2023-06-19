916300ddc39d4fdb55ddfa81813011d63b93576a
palak178 <70845267+palak178@users.noreply.github.com>
2023-06-19 20:41:48 +0530

    Update test_padlock_probe_designer.py

--- Padlock_Probe_miRNA/test_padlock_probe_designer.py
+++ Padlock_Probe_miRNA/test_padlock_probe_designer.py
@@ -1,41 +1,42 @@
-from Bio.Seq import Seq
 import unittest
+from Bio.Seq import Seq
+from io import StringIO
+from contextlib import redirect_stdout
 from Padlock_Probe_Designer import short_formula, long_formula, melting_temp, annealing_temp, index_with_lowest_at, get_padlock_arms
 
-
 class TestMeltingTemp(unittest.TestCase):
-    def test_annealing_temp(self):
-        aseq = "ATCGGCTA"
-        bseq = "CGGCTA"
-        expected_temp = 6.3  # Update the expected temperature based on your calculations
-        result = annealing_temp(aseq, bseq)
-        self.assertAlmostEqual(result, expected_temp, places=2)
 
-    def test_get_padlock_arms(self):
-        miRNA = Seq("ATCGGCTA")
-        expected_arm_a = Seq("ATCGGCTA")  # Update the expected arm_a value to be a Seq object
-        expected_arm_b = Seq("")  # Update the expected arm_b value to be a Seq object
-        result = get_padlock_arms(miRNA)
-        self.assertEqual(str(result[0]), str(expected_arm_a))
-
-    def test_index_with_lowest_at(self):
-        cdna = Seq("ATCGGCTATCGGCTA")  # Convert the input to a Seq object
-        expected_index = 5
-        result = index_with_lowest_at(cdna)
-        self.assertEqual(result, expected_index)
+    def test_short_formula(self):
+        self.assertEqual(short_formula("ACGT"), 16)
 
     def test_long_formula(self):
-        dna_seq = Seq("ATCGGCTATCGGCTA")  # Convert the input to a Seq object
-        expected_tm = 41.94  # Update the expected melting temperature based on your calculations
-        result = long_formula(dna_seq)
-        self.assertAlmostEqual(result, expected_tm, places=2)
+        self.assertEqual(long_formula("ACGT" * 14), 80.94)
 
-    def test_melting_temp_long_sequence(self):
-        dna_seq = Seq("ATCGGCTATCGGCTA")  # Convert the input to a Seq object
-        expected_tm = 41.94  # Update the expected melting temperature based on your calculations
-        result = melting_temp(dna_seq)
-        self.assertAlmostEqual(result, expected_tm, places=2)
+    def test_melting_temp_short_seq(self):
+        self.assertEqual(melting_temp("ACGT"), 16)
 
+    def test_melting_temp_long_seq(self):
+        self.assertEqual(melting_temp("ACGT" * 14), 80.94)
+
+    def test_annealing_temp(self):
+        self.assertAlmostEqual(annealing_temp("ACGT", "TGCA"), 7.24, places=2)
+
+    def test_index_with_lowest_at(self):
+        self.assertEqual(index_with_lowest_at("ACGTTGCA"), 4)
+
+    def test_get_padlock_arms(self):
+        miRNA = Seq("ACGTTGCA")
+        arm1, arm2 = get_padlock_arms(miRNA)
+        self.assertEqual(arm1, Seq("ACGTT"))
+        self.assertEqual(arm2, Seq("GCA"))
+
+    def test_design_padlock_probe(self):
+        # Test the design_padlock_probe function by capturing its output
+        output = StringIO()
+        with redirect_stdout(output):
+            design_padlock_probe()
+        result = output.getvalue().strip()
+        self.assertEqual(result, "Expected output string")
 
 if __name__ == '__main__':
     unittest.main()
